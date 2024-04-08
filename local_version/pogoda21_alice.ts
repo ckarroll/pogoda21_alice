b4f42c811c48bbcefbf38d7275483e8a14742b41
ckarroll <33444308+ckarroll@users.noreply.github.com>
2024-04-08 19:06:58 +0300

    For test on local server

---
+++ local_version/pogoda21_alice.ts
@@ -0,0 +1,21 @@
+import axios from "axios"
+import * as cheerio from "cheerio"
+
+const { Alice } = require('yandex-dialogs-sdk')
+const alice = new Alice();
+
+async function get_weather(){
+  const url = "https://pogoda21.ru/"
+  const response = await axios.get(url)
+  const selector = cheerio.load(response.data)
+  return selector('span:contains("ощущениям") > span').text()
+}
+
+alice.command('', async ctx => {
+  return {
+    text: 'Температура по ощущениям ' + await get_weather(),
+    end_session: true
+  }
+});
+const port = 5000
+alice.listen(port, '/', callback => console.log(port));
